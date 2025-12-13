681405644ca8108dcec6badcbefee9e7e586471c
AzulCastroviejo <azulcastroviejoh3@gmail.com>
2025-12-13 11:19:41 -0300

    eliminacion de duplicado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {ShoppingCart, ChevronRight, ShoppingCart, Tag, Sparkles } from "lucide-react";
+import {ShoppingCart, ChevronRight, Tag, Sparkles } from "lucide-react";
 import api from "../api";
 import Navigation from "../components/Navigation";
 
