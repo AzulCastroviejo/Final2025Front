b25850ca91f028e22f096c48706bf5e174b19bf0
AzulCastroviejo <azulcastroviejoh3@gmail.com>
2026-01-08 12:06:50 -0300

    boton dashboard
    boton dashboard para rol admin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -121,9 +121,8 @@ export default function Home() {
     categories.map(cat => [cat.id_key, cat.name])
   );
 
- /* const user = JSON.parse(localStorage.getItem('user'));
-  const isAdmin = user && user.user.email === 'nosotros.somos.muta@gmail.com';
-*/
+  const user = JSON.parse(localStorage.getItem('user'));
+  const isAdmin =  user?.role === "ADMIN";
 
   useEffect(() => {
     loadHomeData();
@@ -206,13 +205,16 @@ export default function Home() {
               Ver Todos los Productos
               <ChevronRight className="w-5 h-5" />
             </button>
-            <button
+            {user?.role === "ADMIN" && (
+              <button
                 onClick={() => navigate('/dashboard/')}
                 className="ml-4 inline-flex items-center gap-2 px-8 py-4 bg-gradient-to-r from-pink-500 to-purple-500 hover:from-pink-600 hover:to-purple-600 text-white rounded-xl font-semibold text-lg transition-all shadow-lg shadow-pink-500/25 animate-fade-in"
               >
                 Ir al Dashboard
                 <ChevronRight className="w-5 h-5" />
               </button>
+            )}
+
 
           </div>
 
