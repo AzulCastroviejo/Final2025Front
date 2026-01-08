3f2e21f7c5d4f298826bead5f62b2a7595f50a5b
AzulCastroviejo <azulcastroviejoh3@gmail.com>
2026-01-08 15:05:54 -0300

    import api

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { User, Mail, LogOut } from 'lucide-react';
+import api from '../api';
+
 import Navigation from '../components/Navigation';
 
 export default function Profile() {
